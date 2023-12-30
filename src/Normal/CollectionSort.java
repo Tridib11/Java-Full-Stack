8f816f4685a6b15152b5a863fb20e04116b4ef87
Tridib11 <tridibghosh12345@gmail.com>
2023-12-30 12:18:46 +0530

    CollectionSort file added

---
+++ src/Normal/CollectionSort.java
@@ -0,0 +1,27 @@
+package Normal;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class CollectionSort {
+    public static void main(String[] args) {
+        ArrayList<Integer> list=new ArrayList<>();
+        list.add(2);
+        list.add(5);
+        list.add(9);
+        list.add(3);
+        list.add(6);
+
+        //ascending
+        Collections.sort(list);
+        System.out.println(list);
+
+        //Descending
+        Collections.sort(list,Collections.reverseOrder());
+        System.out.println(list);
+
+
+    }
+
+
+}
