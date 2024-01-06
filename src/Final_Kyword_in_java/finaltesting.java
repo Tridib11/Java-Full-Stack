bb2b86a8f87dede796ec1f5d8f43d3ecf0685ced
Tridib11 <tridibghosh12345@gmail.com>
2024-01-06 15:34:34 +0530

    final keyword file added

---
+++ src/Final_Kyword_in_java/finaltesting.java
@@ -0,0 +1,9 @@
+package Final_Kyword_in_java;
+
+public class finaltesting {
+    public static void main(String[] args) {
+        final double PI=3.14;
+//        PI=4;
+        System.out.println(PI);
+    }
+}
