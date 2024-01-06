963a9a3605d26249c28763349265f0b50635aae5
Tridib11 <tridibghosh12345@gmail.com>
2024-01-06 21:44:09 +0530

    Java Object passing files added

---
+++ src/JAVA_OBJECT_PASSING/Car.java
@@ -0,0 +1,8 @@
+package JAVA_OBJECT_PASSING;
+
+public class Car {
+    String name;
+    Car(String name){
+        this.name=name;
+    }
+}
---
+++ src/JAVA_OBJECT_PASSING/Garage.java
@@ -0,0 +1,7 @@
+package JAVA_OBJECT_PASSING;
+
+public class Garage {
+    void park(Car car){
+        System.out.println("The "+car.name+" has been parked");
+    }
+}
