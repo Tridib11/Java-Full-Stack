1aa2b698d152a685651c4235d1a6eb7f2ad8e967
Tridib11 <tridibghosh12345@gmail.com>
2024-01-25 07:05:04 +0530

    linked list files added

---
+++ src/Linkedlist_DSA/Merge_Two_SortedList/ListNode.java
@@ -0,0 +1,38 @@
+package Linkedlist_DSA.Merge_Two_SortedList;
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(){
+
+    }
+    ListNode(int val){
+        this.val=val;
+    }
+    ListNode(int val,ListNode next){
+        this.val=val;
+        this.next=next;
+    }
+
+
+}
+
+class Solution {
+    public LinkedList_Nikhil.ListNode mergeTwoLists(LinkedList_Nikhil.ListNode list1, LinkedList_Nikhil.ListNode list2) {
+        LinkedList_Nikhil.ListNode dummy=new LinkedList_Nikhil.ListNode(0);
+        LinkedList_Nikhil.ListNode curr=dummy;
+        while(list1!=null && list2!=null){
+            if(list1.val<list2.val){
+                curr.next=list1;
+                list1=list1.next;
+            }
+            else{
+                curr.next=list2;
+                list2=list2.next;
+            }
+            curr=curr.next;
+        }
+        curr.next=list1!=null?list1:list2;
+        return dummy.next;
+    }
+}
