f0d22bc51b65ceb6ad97ddf2d63e1addf1bb7c54
Tridib11 <tridibghosh12345@gmail.com>
2024-01-26 08:08:12 +0530

    linked list files added

---
+++ src/LinkedList_Cycle/ListNode.java
@@ -0,0 +1,27 @@
+package LinkedList_Cycle;
+//https://leetcode.com/problems/linked-list-cycle/description/
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+ class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode slowPtr = head;
+        ListNode fastPtr = head;
+        while (slowPtr != null && fastPtr != null && fastPtr.next != null) {
+            slowPtr = slowPtr.next;
+            fastPtr = fastPtr.next.next;
+            if (slowPtr == fastPtr) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+
