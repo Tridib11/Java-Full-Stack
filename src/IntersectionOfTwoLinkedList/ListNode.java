3aebaf9b329a3871891b8d1471aba6396508a5d8
Tridib11 <tridibghosh12345@gmail.com>
2024-01-27 07:35:14 +0530

    linked list files added

---
+++ src/IntersectionOfTwoLinkedList/ListNode.java
@@ -0,0 +1,23 @@
+package IntersectionOfTwoLinkedList;
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode tempA = headA;
+        ListNode tempB = headB;
+        while (tempA != tempB) {
+            tempA = tempA != null ? tempA.next : headB;
+            tempB = tempB != null ? tempB.next : headA;
+        }
+        return tempA;
+    }
+}
+
