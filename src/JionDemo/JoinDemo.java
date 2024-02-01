0fb32f7bbce1b5c0385a16bae5d66edb29ec35bc
Tridib11 <tridibghosh12345@gmail.com>
2024-02-01 19:38:15 +0530

    Multithreading files added

--- src/JionDemo/JoinDemo.java
+++ src/JionDemo/JoinDemo.java
@@ -1,20 +1,41 @@
 package JionDemo;
+
+class MyThread3 extends Thread{
+    static Thread t1;
+
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("MyThread3 Thread Starting");
+            try {
+                t1.join();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
 class DemoThread2 extends Thread{
     @Override
     public void run() {
         for (int i = 0; i < 5; i++) {
             System.out.println("Child Thread Starting");
-        }
-        try{
-            Thread.sleep(1000);
-        }
-        catch (Exception e){
-            e.printStackTrace();
+            try {
+                Thread.sleep(5000);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 }
 public class JoinDemo {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws InterruptedException {
+        DemoThread2 t1=new DemoThread2();
+        t1.start();
+        t1.join();//executed by main thread
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Main thread");
+        }
     }
 }
