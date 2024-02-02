bfd7268efa9a32e90341f8ff3fa0ae9fa1f4e8fd
Tridib11 <tridibghosh12345@gmail.com>
2024-02-02 11:11:50 +0530

    Multithreading files added

---
+++ src/InterCommunicationDemo/InterCommDemo.java
@@ -0,0 +1,27 @@
+package InterCommunicationDemo;
+class ThreadDemo4 extends Thread {
+    int sum = 0;
+    @Override
+    public void run() {
+        synchronized (this) {
+            System.out.println("Child thread start sum");
+            for (int i = 0; i < 100; i++) {
+                sum += i;
+            }
+            System.out.println("Child thread giving notification");
+            this.notify();
+        }
+    }
+}
+public class InterCommDemo {
+    public static void main(String[] args) throws InterruptedException{
+        ThreadDemo4 t1=new ThreadDemo4();
+        t1.start();
+        synchronized (t1){
+            System.out.println("Main thread waiting for t1");
+            t1.wait();
+            System.out.println("Main thread get notification");
+            System.out.println(t1.sum);
+        }
+    }
+}
