1649052396fbc3f785eff45bd54503f3eeb208e2
Tridib11 <tridibghosh12345@gmail.com>
2024-02-20 16:08:01 +0530

    Collection framework files added

--- src/Collection_Framework/vector.java
+++ src/Collection_Framework/vector.java
@@ -1,6 +1,7 @@
 package Collection_Framework;
 
-import java.util.Vector;
+import java.util.*;
+
 //Thread safe
 //default size=10
 // new capacity =10*2
@@ -14,6 +15,28 @@ public class vector {
         System.out.println(vector.capacity());
         vector.add(90);
         System.out.println(vector.capacity());
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
+        Enumeration<Integer> elements=vector.elements();
+        while(elements.hasMoreElements()){
+            Integer integer=elements.nextElement();
+            System.out.println(integer);
+        }
+
+
+        List<Integer> list=new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            list.add(i);
+        }
+        Iterator<Integer> it=list.iterator();
+        while(it.hasNext()){
+            System.out.println(it.next());
+        }
+
+
+
 
     }
 }
