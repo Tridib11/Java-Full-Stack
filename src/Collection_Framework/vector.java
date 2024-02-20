7718c9355e48dc15f17ede199dd338f5e2abcb4e
Tridib11 <tridibghosh12345@gmail.com>
2024-02-20 15:46:50 +0530

    Collection framework files added

---
+++ src/Collection_Framework/vector.java
@@ -0,0 +1,17 @@
+package Collection_Framework;
+
+import java.util.Vector;
+//Thread safe
+public class vector {
+    public static void main(String[] args) {
+        Vector<Integer> vector=new Vector<>();
+        System.out.println(vector.capacity());
+        for (int i = 0; i < 10; i++) {
+            vector.addElement(i);
+        }
+        System.out.println(vector.capacity());
+        vector.add(90);
+        System.out.println(vector.capacity());
+
+    }
+}
