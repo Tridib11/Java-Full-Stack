405834db3fb4d46b456acb365f66cfcd80284f18
Tridib11 <tridibghosh12345@gmail.com>
2024-03-08 04:01:56 +0530

    Stack files added

---
+++ src/Stack_rebuild/NextGreaterElement_GFG.java
@@ -0,0 +1,13 @@
+package Stack_rebuild;
+
+public class NextGreaterElement_GFG {
+    class Solution
+    {
+        //Function to find the next greater element for each element of the array.
+        public static long[] nextLargerElement(long[] arr, int n)
+        {
+
+
+        }
+    }
+}
