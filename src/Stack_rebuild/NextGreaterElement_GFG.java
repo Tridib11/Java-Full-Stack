0d67327b85da7469276e7594ad637fd3a3df1431
Tridib11 <tridibghosh12345@gmail.com>
2024-03-08 04:07:31 +0530

    Stack files added

--- src/Stack_rebuild/NextGreaterElement_GFG.java
+++ src/Stack_rebuild/NextGreaterElement_GFG.java
@@ -1,15 +1,31 @@
 package Stack_rebuild;
 
+import java.util.Stack;
+
 public class NextGreaterElement_GFG {
     class Solution
     {
         //Function to find the next greater element for each element of the array.
         public static long[] nextLargerElement(long[] arr, int n)
         {
-            int[] nge=new int[n];
+           long[] nge=new long[n];
+            Stack<Long> stack=new Stack<>();
             for(int i=n-1;i>=0;i--){
-
+                while(!stack.isEmpty() && stack.peek()<=arr[i]){
+                    stack.pop();
+                }
+                if(i<n){
+                    if(!stack.isEmpty()){
+                        nge[i]=stack.peek();
+                    }
+                    else{
+                        nge[i]=-1;
+                    }
+                }
+                stack.push(arr[i]);
             }
+            return nge;
+
         }
     }
 }
