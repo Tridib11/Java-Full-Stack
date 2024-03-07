b36c10f603126606ce4605a9d8752e56b6619c0b
Tridib11 <tridibghosh12345@gmail.com>
2024-03-08 03:59:50 +0530

    Stack files added

--- src/Stack_rebuild/NextGreaterElement.java
+++ src/Stack_rebuild/NextGreaterElement.java
@@ -1,7 +1,7 @@
 package Stack_rebuild;
 
 import java.util.Stack;
-
+//https://leetcode.com/problems/next-greater-element-ii/description/
 public class NextGreaterElement {
     class Solution {
         public int[] nextGreaterElements(int[] nums) {
@@ -9,8 +9,20 @@ public class NextGreaterElement {
             int[] nge=new int[n];
             Stack<Integer> stack=new Stack<>();
             for(int i=2*n-1;i>=0;i--){
-                while()
+                while(!stack.isEmpty() && stack.peek()<=nums[i%n]){
+                    stack.pop();
+                }
+                if(i<n){
+                    if(!stack.isEmpty()){
+                        nge[i]=stack.peek();
+                    }
+                    else{
+                        nge[i]=-1;
+                    }
+                }
+                stack.push(nums[i%n]);
             }
+            return nge;
         }
     }
 
