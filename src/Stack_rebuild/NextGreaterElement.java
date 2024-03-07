d5373b546f3eb141e9f5a26d118508677120136b
Tridib11 <tridibghosh12345@gmail.com>
2024-03-08 03:48:39 +0530

    Stack files added

--- src/Stack_rebuild/NextGreaterElement.java
+++ src/Stack_rebuild/NextGreaterElement.java
@@ -1,6 +1,29 @@
 package Stack_rebuild;
 
-public class NextGreaterElement {
+import java.util.Stack;
 
+public class NextGreaterElement {
+    class Solution {
+        public int[] nextGreaterElements(int[] nums) {
+            int n=nums.length;
+            int[] nge=new int[n];
+            Stack<Integer> stack=new Stack<>();
+            for(int i=2*n-1;i>=0;i--){
+                while(!stack.isEmpty() && stack.peek()<=nums[i%n]){
+                    stack.pop();
+                }
+                if(i<n){
+                    if(!stack.isEmpty()){
+                        nge[i]=stack.peek();
+                    }
+                    else {
+                        nge[i]=-1;
+                    }
+                }
+                stack.push(nums[i%n]);
+            }
+            return nge;
+        }
+    }
 
 }
