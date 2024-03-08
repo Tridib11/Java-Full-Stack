3bb814c9beea9581f57797ad219d0f130bd73f49
Tridib11 <tridibghosh12345@gmail.com>
2024-03-08 05:44:30 +0530

    Stack files added

--- src/Stack_rebuild/ValidStackSequences_leetcode.java
+++ src/Stack_rebuild/ValidStackSequences_leetcode.java
@@ -1,13 +1,26 @@
 package Stack_rebuild;
 
+import java.util.Stack;
+
 /*
-Company Tags                : MICROSOFT
-Leetcode Link               : https://leetcode.com/problems/removing-stars-from-a-string/
+Company Tags   : MICROSOFT
+Leetcode Link  : https://leetcode.com/problems/removing-stars-from-a-string/
  */
 public class ValidStackSequences_leetcode {
     class Solution {
         public boolean validateStackSequences(int[] pushed, int[] popped) {
-
+            int n=pushed.length;
+            int i=0,j=0;
+            Stack<Integer> stack=new Stack<>();
+            while(i<n && j<n){
+                stack.push(pushed[i]);
+                while(!stack.isEmpty() && stack.peek()==popped[j]){
+                    stack.pop();
+                    j++;
+                }
+                i++;
+            }
+            return stack.isEmpty();
         }
     }
 }
