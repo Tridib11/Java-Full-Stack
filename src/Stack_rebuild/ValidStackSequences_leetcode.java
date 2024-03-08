f5a57d91226cabc20a92ea8f31d36d77a1da6c71
Tridib11 <tridibghosh12345@gmail.com>
2024-03-08 05:36:51 +0530

    Stack files added

---
+++ src/Stack_rebuild/ValidStackSequences_leetcode.java
@@ -0,0 +1,13 @@
+package Stack_rebuild;
+
+/*
+Company Tags                : MICROSOFT
+Leetcode Link               : https://leetcode.com/problems/removing-stars-from-a-string/
+ */
+public class ValidStackSequences_leetcode {
+    class Solution {
+        public boolean validateStackSequences(int[] pushed, int[] popped) {
+
+        }
+    }
+}
