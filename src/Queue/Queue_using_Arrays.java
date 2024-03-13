1d43bbbb2d4d63768d86c0bdd36995132c7ad444
Tridib11 <tridibghosh12345@gmail.com>
2024-03-13 13:04:27 +0530

    Queue files added

--- src/Queue/Queue_using_Arrays.java
+++ src/Queue/Queue_using_Arrays.java
@@ -4,9 +4,24 @@ public class Queue_using_Arrays {
     static class Queue{
         static int[] arr;
         static int size;
+        static int rear;
         Queue(int n){
             arr=new int[n];
             size=n;
+            rear=-1;
+        }
+
+        public static boolean isEmpty(){
+            return rear==-1;
+        }
+
+        public static void add(int data){
+            if(rear==size-1){
+                System.out.println("Queue is full");
+                return;
+            }
+            rear+=1;
+            arr[rear]=data;
         }
     }
 }
