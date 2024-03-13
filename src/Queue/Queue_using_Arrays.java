c8569499e7f150113117386fac4abee2cfbabcfc
Tridib11 <tridibghosh12345@gmail.com>
2024-03-13 13:02:30 +0530

    Queue files added

---
+++ src/Queue/Queue_using_Arrays.java
@@ -0,0 +1,12 @@
+package Queue;
+
+public class Queue_using_Arrays {
+    static class Queue{
+        static int[] arr;
+        static int size;
+        Queue(int n){
+            arr=new int[n];
+            size=n;
+        }
+    }
+}
