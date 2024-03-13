f264d5603491d1c50d18aeea559ea9a4e82e32f0
Tridib11 <tridibghosh12345@gmail.com>
2024-03-13 13:27:34 +0530

    Queue files added

--- src/Queue/Queue_using_Arrays.java
+++ src/Queue/Queue_using_Arrays.java
@@ -49,4 +49,15 @@ public class Queue_using_Arrays {
         }
 
     }
+
+    public static void main(String[] args) {
+        Queue q=new Queue(5);
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        while(!q.isEmpty()){
+            System.out.println(q.peek());
+            q.remove();
+        }
+    }
 }
