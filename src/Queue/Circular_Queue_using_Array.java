bae401ff523d2559334d20d1f2d9360d313142cf
Tridib11 <tridibghosh12345@gmail.com>
2024-03-14 18:12:32 +0530

    Queue files added

--- src/Queue/Circular_Queue_using_Array.java
+++ src/Queue/Circular_Queue_using_Array.java
@@ -9,23 +9,33 @@ public class Circular_Queue_using_Array {
         static int[] arr;
         static int size;
         static int rear;
+        static int front;
         Queue(int n){
             arr=new int[n];
             size=n;
             rear=-1;
+            front=-1;
         }
 
         public static boolean isEmpty(){
-            return rear==-1;
+            return rear==-1 && front==-1;
+        }
+
+        public static boolean isFull(){
+            return (rear+1)%size==front;
         }
 
         //add
         public static void add(int data){
-            if(rear==size-1){
+            if(isFull()){
                 System.out.println("Queue is full");
                 return;
             }
-            rear+=1;
+            //adding first element
+            if(front==-1){
+                front=0;
+            }
+            rear=(rear+1)%size;
             arr[rear]=data;
         }
 
@@ -35,12 +45,14 @@ public class Circular_Queue_using_Array {
                 System.out.println("Empty queue");
                 return -1;
             }
-            int front=arr[0];
-            for(int i=0;i<rear;i++){
-                arr[i]=arr[i+1];
+            int result=arr[front];
+            //last element delete
+            if(rear==front){
+                rear=front=-1;
+            }else{
+                front=(front+1)%size;
             }
-            rear-=1;
-            return front;
+            return result;
         }
 
         //peek
@@ -49,7 +61,7 @@ public class Circular_Queue_using_Array {
                 System.out.println("Empty queue");
                 return -1;
             }
-            return arr[0];
+            return arr[front];
         }
 
     }
