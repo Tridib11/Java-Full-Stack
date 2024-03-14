8283f395527f8c8133b1a5f2a1fd762df439d3cd
Tridib11 <tridibghosh12345@gmail.com>
2024-03-14 18:32:17 +0530

    Queue files added

--- src/Queue/Circular_Queue_using_Array.java
+++ src/Queue/Circular_Queue_using_Array.java
@@ -67,10 +67,14 @@ public class Circular_Queue_using_Array {
     }
 
     public static void main(String[] args) {
-        Queue.Queue_using_Arrays.Queue q=new Queue.Queue_using_Arrays.Queue(5);
+        Queue q=new Queue(3);
         q.add(1);
         q.add(2);
         q.add(3);
+        System.out.println(q.remove());
+        q.add(4);
+        System.out.println(q.remove());
+        q.add(5);
         while(!q.isEmpty()){
             System.out.println(q.peek());
             q.remove();
