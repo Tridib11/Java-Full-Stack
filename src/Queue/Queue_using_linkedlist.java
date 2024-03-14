83ca1922c0053fb128e6ca41c14a763c88be6b83
Tridib11 <tridibghosh12345@gmail.com>
2024-03-14 18:58:28 +0530

    Queue files added

---
+++ src/Queue/Queue_using_linkedlist.java
@@ -0,0 +1,80 @@
+package Queue;
+
+public class Queue_using_linkedlist {
+
+    static class Node{
+        int data;
+        Node next;
+        Node(int data){
+          this.data=data;
+          this.next=null;
+        }
+    }
+    static class Queue{
+        static Node head=null;
+        static Node tail=null;
+
+
+
+        public static boolean isEmpty(){
+            return head==null && tail==null;
+        }
+
+
+        //add
+        public static void add(int data){
+            if(isFull()){
+                System.out.println("Queue is full");
+                return;
+            }
+            //adding first element
+            if(front==-1){
+                front=0;
+            }
+            rear=(rear+1)%size;
+            arr[rear]=data;
+        }
+
+        //remove
+        public static int remove(){
+            if(isEmpty()){
+                System.out.println("Empty queue");
+                return -1;
+            }
+            int result=arr[front];
+            //last element delete
+            if(rear==front){
+                rear=front=-1;
+            }else{
+                front=(front+1)%size;
+            }
+            return result;
+        }
+
+        //peek
+        public static int peek(){
+            if(isEmpty()){
+                System.out.println("Empty queue");
+                return -1;
+            }
+            return arr[front];
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Queue.Circular_Queue_using_Array.Queue q=new Queue.Circular_Queue_using_Array.Queue(3);
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        System.out.println(q.remove());
+        q.add(4);
+        System.out.println(q.remove());
+        q.add(5);
+        while(!q.isEmpty()){
+            System.out.println(q.peek());
+            q.remove();
+        }
+    }
+
+}
