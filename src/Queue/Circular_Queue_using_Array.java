99d8bcfcaa3ed87f61bf09914c63eab8a909454f
Tridib11 <tridibghosh12345@gmail.com>
2024-03-14 17:35:37 +0530

    Queue files added

---
+++ src/Queue/Circular_Queue_using_Array.java
@@ -0,0 +1,67 @@
+package Queue;
+
+/*
+add(O(1))
+remove(O(1))
+ */
+public class Circular_Queue_using_Array {
+    static class Queue{
+        static int[] arr;
+        static int size;
+        static int rear;
+        Queue(int n){
+            arr=new int[n];
+            size=n;
+            rear=-1;
+        }
+
+        public static boolean isEmpty(){
+            return rear==-1;
+        }
+
+        //add
+        public static void add(int data){
+            if(rear==size-1){
+                System.out.println("Queue is full");
+                return;
+            }
+            rear+=1;
+            arr[rear]=data;
+        }
+
+        //remove
+        public static int remove(){
+            if(isEmpty()){
+                System.out.println("Empty queue");
+                return -1;
+            }
+            int front=arr[0];
+            for(int i=0;i<rear;i++){
+                arr[i]=arr[i+1];
+            }
+            rear-=1;
+            return front;
+        }
+
+        //peek
+        public static int peek(){
+            if(isEmpty()){
+                System.out.println("Empty queue");
+                return -1;
+            }
+            return arr[0];
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Queue.Queue_using_Arrays.Queue q=new Queue.Queue_using_Arrays.Queue(5);
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        while(!q.isEmpty()){
+            System.out.println(q.peek());
+            q.remove();
+        }
+    }
+}
