af51ebde5d6444c7687a29500e591aea8cc54e27
Tridib11 <tridibghosh12345@gmail.com>
2024-03-15 15:48:56 +0530

    Queue files added

--- src/Queue/Queuse_using_JCF.java
+++ src/Queue/Queuse_using_JCF.java
@@ -1,11 +1,13 @@
 package Queue;
 
+import java.util.ArrayDeque;
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class Queuse_using_JCF {
     public static void main(String[] args) {
-        Queue<Integer> q=new LinkedList<>();
+//        Queue<Integer> q=new LinkedList<>();
+        Queue<Integer> q=new ArrayDeque<>();
         q.add(1);
         q.add(2);
         q.add(3);
