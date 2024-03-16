771b73a9259dd9a625357286e4fba9d14b6b28ca
Tridib11 <tridibghosh12345@gmail.com>
2024-03-17 00:35:21 +0530

    Queue files added

--- src/Queue/First_unique_charavter_in_a_String.java
+++ src/Queue/First_unique_charavter_in_a_String.java
@@ -1,5 +1,33 @@
 package Queue;
-
+import java.util.*;
 public class First_unique_charavter_in_a_String {
+    class Solution {
+        public int firstUniqChar(String str) {
+            Queue<Character> queue = new LinkedList<>();
+            int[] freq = new int[26];
 
+            // Iterate through the string.
+            for (int i = 0; i < str.length(); i++) {
+                char ch = str.charAt(i);
+                queue.add(ch);
+                freq[ch - 'a']++;
+
+                // Remove characters from the queue until finding a non-repeating character.
+                while (!queue.isEmpty() && freq[queue.peek() - 'a'] > 1) {
+                    queue.remove();
+                }
+            }
+
+            // Iterate through the string again to find the index of the first non-repeating character.
+            for (int i = 0; i < str.length(); i++) {
+                if (freq[str.charAt(i) - 'a'] == 1) {
+                    return i;
+                }
+            }
+
+            // If no non-repeating character is found, return -1.
+            return -1;
+
+        }
+    }
 }
