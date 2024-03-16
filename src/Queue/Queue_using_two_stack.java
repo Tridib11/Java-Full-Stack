298311476c3776102865a94a4d82cfab4f4293f5
Tridib11 <tridibghosh12345@gmail.com>
2024-03-16 16:32:44 +0530

    Queue files added

--- src/Queue/Queue_using_two_stack.java
+++ src/Queue/Queue_using_two_stack.java
@@ -1,4 +1,23 @@
 package Queue;
 
+import java.util.Stack;
+
 public class Queue_using_two_stack {
+    static class Queue{
+        static Stack<Integer> s1=new Stack<>();
+        static Stack<Integer> s2=new Stack<>();
+
+        public static boolean isEmpty(){
+            return s1.isEmpty();
+        }
+        public static void add(int data){
+            while(!s1.isEmpty()){
+
+            }
+        }
+
+    }
+    public static void main(String[] args) {
+
+    }
 }
