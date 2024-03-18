b0fda2653b70d1c71db668c63841fe328778ae98
Tridib11 <tridibghosh12345@gmail.com>
2024-03-18 12:13:31 +0530

    Queue files added

--- src/Queue/Deque_test.java
+++ src/Queue/Deque_test.java
@@ -1,5 +1,16 @@
 package Queue;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 public class Deque_test {
+    public static void main(String[] args) {
+        Deque<Integer> q=new LinkedList<>();
+        q.addFirst(1);
+        q.addFirst(2);
+        System.out.println(q);
+        q.removeFirst();
+        System.out.println(q);
+    }
 
 }
