fab7a338761471258992097b3fad8ef2e447810a
Tridib11 <tridibghosh12345@gmail.com>
2024-03-18 11:33:38 +0530

    Queue files added

---
+++ src/Queue/Interleave_the_First_Half_of_the_Queue_with_Second_Half.java
@@ -0,0 +1,30 @@
+package Queue;
+import java.util.*;
+public class Interleave_the_First_Half_of_the_Queue_with_Second_Half {
+
+    class Solution {
+        public static ArrayList<Integer> rearrangeQueue(int N, Queue<Integer> q) {
+            // code here
+            Queue<Integer> firstHalf = new LinkedList<>();
+            ArrayList<Integer> interleavedList = new ArrayList<>();
+
+            int size = q.size();
+
+            for (int i = 0; i < size / 2; i++) {
+                firstHalf.add(q.remove());
+            }
+
+            while (!firstHalf.isEmpty()) {
+                interleavedList.add(firstHalf.remove());
+                interleavedList.add(q.remove());
+            }
+
+            // Add any remaining elements from the original queue
+            while (!q.isEmpty()) {
+                interleavedList.add(q.remove());
+            }
+
+            return interleavedList;
+        }
+    }
+}
