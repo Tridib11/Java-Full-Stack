5a8279e3eb89d12cdcd20bd7fdcae88f65d049c1
Tridib11 <tridibghosh12345@gmail.com>
2024-03-18 11:46:36 +0530

    Queue files added

--- src/Queue/queue_reversal.java
+++ src/Queue/queue_reversal.java
@@ -1,5 +1,5 @@
 package Queue;
-
+https://www.geeksforgeeks.org/problems/queue-reversal/1?itm_source=geeksforgeeks&itm_medium=article&itm_campaign=bottom_sticky_on_article
 public class queue_reversal {
 
 }
