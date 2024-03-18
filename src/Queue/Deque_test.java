5d2349388e188408d446aa77e196721a0933d387
Tridib11 <tridibghosh12345@gmail.com>
2024-03-18 12:15:47 +0530

    Queue files added

--- src/Queue/Deque_test.java
+++ src/Queue/Deque_test.java
@@ -9,6 +9,7 @@ public class Deque_test {
         q.addFirst(1);
         q.addFirst(2);
         System.out.println(q);
+        q.addLast(12);
         q.removeFirst();
         System.out.println(q);
     }
