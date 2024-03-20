73152eddf3f9b3a5c2b22686a4b7833384a5d37a
Tridib11 <tridibghosh12345@gmail.com>
2024-03-21 05:29:30 +0530

    Greedy algo files added

--- src/Greedy_Alogorithm/Sorted_Activities.java
+++ src/Greedy_Alogorithm/Sorted_Activities.java
@@ -3,7 +3,7 @@ package Greedy_Alogorithm;
 import java.util.ArrayList;
 
 public class Sorted_Activities {
-    public static void main(String[] args) {
+    public static void main(String[] args) { //O(n)
         int[] start={1,3,0,5,8,5};
         int[] end={2,4,6,7,9,9};
         int maxAct=0;
