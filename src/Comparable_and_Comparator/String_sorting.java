aa2e6a8fcca501265ae5579224c41b7bdc43733d
Tridib11 <tridibghosh12345@gmail.com>
2024-03-21 17:57:39 +0530

    Comparable and comparator files added

--- src/Comparable_and_Comparator/String_sorting.java
+++ src/Comparable_and_Comparator/String_sorting.java
@@ -5,6 +5,11 @@ import java.util.ArrayList;
 public class String_sorting {
     public static void main(String[] args) {
         ArrayList<String> musicList=new ArrayList<>();
+        musicList.add("Zara zara");
+        musicList.add("besabriyan");
+        musicList.add("Kaise hua");
+        musicList.add("aeisa desh he mera");
+        musicList.add("koi lauta do wo pyare pyare din");
 
     }
 }
