e212f4cc1eb0dfa10564b712a93abea7bccceda9
Tridib11 <tridibghosh12345@gmail.com>
2024-03-21 05:41:12 +0530

    Greedy algo files added

--- src/Greedy_Alogorithm/unSorted_Activities.java
+++ src/Greedy_Alogorithm/unSorted_Activities.java
@@ -1,4 +1,15 @@
 package Greedy_Alogorithm;
 
 public class unSorted_Activities {
+    public static void main(String[] args) {
+        int[] start={1,3,0,5,8,5};
+        int[] end={2,4,6,7,9,9};
+        int activities[][]=new int[start.length][3];
+        for(int i=0;i<start.length;i++){
+            activities[i][0]=i;
+            activities[i][1]=start[1];
+            activities[i][2]=end[1];
+        }
+
+    }
 }
