b76a7d2dd05c5badd745c651e3097bd54ed5fbfe
Tridib11 <tridibghosh12345@gmail.com>
2024-03-21 18:04:02 +0530

    Comparable and comparator files added

--- src/Comparable_and_Comparator/String_sorting.java
+++ src/Comparable_and_Comparator/String_sorting.java
@@ -1,17 +1,25 @@
 package Comparable_and_Comparator;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class String_sorting {
     public static void main(String[] args) {
         ArrayList<String> musicList=new ArrayList<>();
-        musicList.add("Zara zara");
+        musicList.add("zara zara");
         musicList.add("besabriyan");
-        musicList.add("Kaise hua");
+        musicList.add("kaise hua");
         musicList.add("aeisa desh he mera");
         musicList.add("koi lauta do wo pyare pyare din");
-        for(String music:musicList){
-            System.out.println(music);
+        for(String temp:musicList){
+            System.out.println(temp);
         }
+        System.out.println("---------------");
+//        Collections.sort(musicList, String.CASE_INSENSITIVE_ORDER);
+        Collections.sort(musicList);
+        for(String temp:musicList){
+            System.out.println(temp);
+        }
+
     }
 }
