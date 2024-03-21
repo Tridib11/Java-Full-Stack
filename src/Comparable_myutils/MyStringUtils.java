66abd5d072f601e18c29171abe99a3cb0269cc67
Tridib11 <tridibghosh12345@gmail.com>
2024-03-21 18:18:04 +0530

    Comparable and comparator files added

--- src/Comparable_myutils/MyStringUtils.java
+++ src/Comparable_myutils/MyStringUtils.java
@@ -3,9 +3,5 @@ package Comparable_myutils;
 import java.util.ArrayList;
 
 public class MyStringUtils {
-    public static void iterateList(ArrayList<String> musicList) {
-        for(String temp: musicList){
-            System.out.println(temp);
-        }
-    }
+
 }
