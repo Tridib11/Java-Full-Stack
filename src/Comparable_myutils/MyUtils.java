bd9822a135b475195c25ee439499bb6449b8e92a
Tridib11 <tridibghosh12345@gmail.com>
2024-03-21 18:18:15 +0530

    Comparable and comparator files added

---
+++ src/Comparable_myutils/MyUtils.java
@@ -0,0 +1,16 @@
+package Comparable_myutils;
+
+import java.util.ArrayList;
+
+public class MyUtils {
+    public static void iterateIntegerList(ArrayList<Integer> integerList) {
+        for(int num: integerList){
+            System.out.println(num);
+        }
+    }
+    public static void iterateStringList(ArrayList<String> musicList) {
+        for(String temp: musicList){
+            System.out.println(temp);
+        }
+    }
+}
