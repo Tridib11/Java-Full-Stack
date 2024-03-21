79995c0eaa48edb9bd4718ea450c41efa92a17f1
Tridib11 <tridibghosh12345@gmail.com>
2024-03-21 18:25:26 +0530

    Comparable and comparator files added

--- src/Comparable_myutils/MyUtils.java
+++ src/Comparable_myutils/MyUtils.java
@@ -3,8 +3,8 @@ package Comparable_myutils;
 import java.util.ArrayList;
 
 public class MyUtils {
-    public static <T> void iterateIntegerList(ArrayList<T> integerList) {
-        for(T num: integerList){
+    public static <T> void iterateList(ArrayList<T> List) {
+        for(T num: List){
             System.out.println(num);
         }
     }
