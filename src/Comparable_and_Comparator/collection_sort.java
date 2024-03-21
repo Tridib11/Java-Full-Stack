510609e39e9a15645c49a59be8779413db641c42
Tridib11 <tridibghosh12345@gmail.com>
2024-03-21 17:48:54 +0530

    Comparable and comparator files added

--- src/Comparable_and_Comparator/collection_sort.java
+++ src/Comparable_and_Comparator/collection_sort.java
@@ -1,7 +1,9 @@
 package Comparable_and_Comparator;
 
+import java.util.ArrayList;
+
 public class collection_sort {
     public static void main(String[] args) {
-
+        ArrayList<Integer> integerList=new ArrayList<>();
     }
 }
