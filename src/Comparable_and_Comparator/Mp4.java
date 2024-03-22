5f4ad6e1c519b318408df28313f63fb94373d333
Tridib11 <tridibghosh12345@gmail.com>
2024-03-22 21:46:58 +0530

    Comparable and comparator files added

--- src/Comparable_and_Comparator/Mp4.java
+++ src/Comparable_and_Comparator/Mp4.java
@@ -1,4 +1,10 @@
 package Comparable_and_Comparator;
 
+import java.util.ArrayList;
+
 public class Mp4 {
+    public static void main(String[] args) {
+        ArrayList<Song> musicList=new ArrayList<Song>();
+
+    }
 }
