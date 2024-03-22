71ae228ea5e4bef2f0498e4f62818abc7f9bbf72
Tridib11 <tridibghosh12345@gmail.com>
2024-03-22 21:44:17 +0530

    Comparable and comparator files added

--- src/Comparable_api/Songs.java
+++ src/Comparable_api/Songs.java
@@ -4,4 +4,28 @@ public class Songs {
     private String title;
     private String artist;
     private Integer year;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
 }
