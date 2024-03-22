9b70a9ce2c4717620b4aca10b1af3546447ab217
Tridib11 <tridibghosh12345@gmail.com>
2024-03-22 21:52:45 +0530

    Comparable and comparator files added

--- src/Comparable_and_Comparator/Mp4.java
+++ src/Comparable_and_Comparator/Mp4.java
@@ -6,6 +6,13 @@ import java.util.ArrayList;
 
 public class Mp4 {
     public static void main(String[] args) {
+        Songs song1=new Songs("Take me ","Michael",2013);
+        Songs song2=new Songs("Move me ","Tridib",2012);
+        Songs song3=new Songs("Touch me ","Samm",2011);
+        Songs song4=new Songs("kick me ","Rahul",2005);
+        Songs song5=new Songs("help me ","Rohan",2009);
+
+
         ArrayList<Songs> musicList=new ArrayList<>();
 
     }
