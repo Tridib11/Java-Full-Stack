ce818bec166b42237b89206f95c96860f9ad49af
Tridib11 <tridibghosh12345@gmail.com>
2024-03-24 01:50:41 +0530

    Comparable and comparator files added

--- src/Comparators_and_Comparable_Anuj/MainClass.java
+++ src/Comparators_and_Comparable_Anuj/MainClass.java
@@ -1,4 +1,16 @@
 package Comparators_and_Comparable_Anuj;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainClass {
+    public static void main(String[] args) {
+        List<Students> students=new ArrayList<>();
+        students.add(new Students(23,"Ram"));
+        students.add(new Students(12,"Rohan"));
+        students.add(new Students(13,"Kisan"));
+        students.add(new Students(29,"Rahul"));
+        students.add(new Students(1,"Pramanik"));
+
+    }
 }
