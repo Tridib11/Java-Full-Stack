31683e2a3ddb8a48dea364bd0c3f5f982a2c8f88
Tridib11 <tridibghosh12345@gmail.com>
2024-03-24 01:48:57 +0530

    Comparable and comparator files added

--- src/Comparators_and_Comparable_Anuj/Students.java
+++ src/Comparators_and_Comparable_Anuj/Students.java
@@ -3,5 +3,33 @@ package Comparators_and_Comparable_Anuj;
 public class Students {
     int marks;
     String name;
+    public Students(int marks,String name){
+        super();
+        this.marks=marks;
+        this.name=name;
+    }
 
+    @Override
+    public String toString() {
+        return "Students{" +
+                "marks=" + marks +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public int getMarks() {
+        return marks;
+    }
+
+    public void setMarks(int marks) {
+        this.marks = marks;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
