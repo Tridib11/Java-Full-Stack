81c2d8026ac22a31932c1906814c4abe9756c0a7
Tridib11 <tridibghosh12345@gmail.com>
2024-03-24 01:45:02 +0530

    Comparable and comparator files added

--- src/Comparable_and_Comparator/Mp4.java
+++ src/Comparable_and_Comparator/Mp4.java
@@ -1,28 +1,28 @@
-package Comparable_and_Comparator;
-
-import Comparable_api.Songs;
-import Comparable_myutils.MyUtils;
-
-import java.util.ArrayList;
-import java.util.Collections;
-
-public class Mp4 {
-    public static void main(String[] args) {
-        Songs song1=new Songs("take me ","Michael",2013);
-        Songs song2=new Songs("move me ","Tridib",2012);
-        Songs song3=new Songs("touch me ","Samm",2011);
-        Songs song4=new Songs("kick me ","Rahul",2005);
-        Songs song5=new Songs("help me ","Rohan",2009);
-
-
-        ArrayList<Songs> musicList=new ArrayList<>();
-        musicList.add(song1);
-        musicList.add(song2);
-        musicList.add(song3);
-        musicList.add(song4);
-        musicList.add(song5);
-        MyUtils.iterateList(musicList);
-        Collections.sort(musicList);
-
-    }
-}
+//package Comparable_and_Comparator;
+//
+//import Comparable_api.Songs;
+//import Comparable_myutils.MyUtils;
+//
+//import java.util.ArrayList;
+//import java.util.Collections;
+//
+//public class Mp4 {
+//    public static void main(String[] args) {
+//        Songs song1=new Songs("take me ","Michael",2013);
+//        Songs song2=new Songs("move me ","Tridib",2012);
+//        Songs song3=new Songs("touch me ","Samm",2011);
+//        Songs song4=new Songs("kick me ","Rahul",2005);
+//        Songs song5=new Songs("help me ","Rohan",2009);
+//
+//
+//        ArrayList<Songs> musicList=new ArrayList<>();
+//        musicList.add(song1);
+//        musicList.add(song2);
+//        musicList.add(song3);
+//        musicList.add(song4);
+//        musicList.add(song5);
+//        MyUtils.iterateList(musicList);
+//        Collections.sort(musicList);
+//
+//    }
+//}
