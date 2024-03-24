0655879e2e87a855ea9224e53b25ac6e85a5933d
Tridib11 <tridibghosh12345@gmail.com>
2024-03-25 04:35:20 +0530

    Greedy algo added

--- src/Greedy_Alogorithm/Job_Sequencing_problem.java
+++ src/Greedy_Alogorithm/Job_Sequencing_problem.java
@@ -1,5 +1,55 @@
 package Greedy_Alogorithm;
 
+import java.util.Arrays;
+
+class Job {
+    int id, profit, deadline;
+    Job(int x, int y, int z){
+        this.id = x;
+        this.deadline = y;
+        this.profit = z;
+    }
+}
+class Solution
+{
+    //Function to find the maximum profit and the number of jobs done.
+    int[] JobScheduling(Job arr[], int n)
+    {
+        Arrays.sort(arr,(a,b)->(b.profit-a.profit));//storing in descending order of profit
+        int max=0;
+        for (int i = 0; i < n; i++) {
+            if(arr[i].deadline>max){
+                max=arr[i].deadline;
+            }
+        }
+        int[] result=new int[max+1];
+        for(int i=0;i<max+1;i++){
+            result[i]=-1;
+        }
+        int countJobs=0,jobProfit=0;
+        for(int i=0;i<n;i++){
+            for(int j=arr[i].deadline;j>0;j--){
+                if(result[j]==-1){
+                    result[j]=i;
+                    countJobs++;
+                    jobProfit+=arr[i].profit;
+                    break;
+                }
+            }
+        }
+        int ans[]=new int[2];
+        ans[0]=countJobs;
+        ans[1]=jobProfit;
+        return ans;
+
+
+
+    }
+}
+
+
+
+
 public class Job_Sequencing_problem {
     public static void main(String[] args) {
 
