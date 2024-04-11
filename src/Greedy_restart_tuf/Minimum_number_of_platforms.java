70e7140f3b5a62edd4928885d6b4878fe38dcaf3
Tridib11 <tridibghosh12345@gmail.com>
2024-04-11 19:42:34 +0530

    Greedy algo added

--- src/Greedy_restart_tuf/Minimum_number_of_platforms.java
+++ src/Greedy_restart_tuf/Minimum_number_of_platforms.java
@@ -1,6 +1,24 @@
 package Greedy_restart_tuf;
 
+import java.util.Arrays;
+
 public class Minimum_number_of_platforms {
+
+    static int platform(int[] arr,int[] dep,int n){
+        Arrays.sort(arr);
+        Arrays.sort(dep);
+        int platform_needed=1,result=1;
+        int i=1,j=0;
+        while(i<n && j<n){
+            if(arr[i]<=dep[j]){
+                platform_needed++;
+                i++;
+            }
+            else if (arr[i]>dep[j]){
+
+            }
+        }
+    }
     public static void main(String[] args) {
 
     }
