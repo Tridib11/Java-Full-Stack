aba09dcfd12345c91e1a75bcf7fbed1f4a4c321e
Tridib11 <tridibghosh12345@gmail.com>
2024-04-11 08:02:21 +0530

    Greedy algo added

--- src/Greedy_restart_tuf/N_meetings_in_one_room.java
+++ src/Greedy_restart_tuf/N_meetings_in_one_room.java
@@ -1,4 +1,39 @@
 package Greedy_restart_tuf;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+
+class Meeting{
+    int start;
+    int end;
+    int pos;
+
+    Meeting(int start,int end,int pos){
+        this.start=start;
+        this.end=end;
+        this.pos=pos;
+    }
+}
+
+class MeetingComparator implements Comparator<Meeting>{
+    @Override
+    public int compare(Meeting m1,Meeting m2){
+        if(m1.end<m2.end){
+            return -1;
+        }
+        else if(m1.end>m2.end){
+            return 1;
+        } else if (m1.pos < m2.pos) {
+            return -1;
+        }
+        return 1;
+    }
+}
 public class N_meetings_in_one_room {
+    static void maxMeetings(int start[],int end[],int n){
+        ArrayList<Meeting> meet=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            meet.add(new Meeting(start[i],end[i],i+1 ));
+        }
+    }
 }
