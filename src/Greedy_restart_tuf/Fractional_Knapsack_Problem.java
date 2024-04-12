adf04a2f14430811d0c2a4f3ebda018a2b142db3
Tridib11 <tridibghosh12345@gmail.com>
2024-04-12 22:28:25 +0530

    Greedy algo added

--- src/Greedy_restart_tuf/Fractional_Knapsack_Problem.java
+++ src/Greedy_restart_tuf/Fractional_Knapsack_Problem.java
@@ -1,4 +1,37 @@
 package Greedy_restart_tuf;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
+class Item{
+    int value,weight;
+    Item(int value,int weight){
+        this.value=value;
+        this.weight=weight;
+    }
+}
+
+class itemComparator implements Comparator<Item>{
+    @Override
+    public int compare(Item a, Item b) {
+        double r1=(double)(a.value)/(double)(a.weight);
+        double r2=(double)(b.value)/(double)(b.weight);
+        if(r1<r2) return 1;
+        else if(r1>r2) return -1;
+        else return 0;
+    }
+
+}
 public class Fractional_Knapsack_Problem {
+    static double fractionalKnapsack(int W,Item[] arr,int n){
+        Arrays.sort(arr,new itemComparator());
+        int currWeight=0;
+        double finalWeight=0.0;
+        for(int i=0;i<n;i++){
+            if(currWeight+arr[i].weight<W){
+                currWeight+=arr[i].weight;
+                finalWeight+=arr[i].value;
+            }
+        }
+    }
 }
