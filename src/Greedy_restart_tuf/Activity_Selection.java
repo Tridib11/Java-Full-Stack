e92aa74a424c7e194cdd9a6a400c7279785c560e
Tridib11 <tridibghosh12345@gmail.com>
2024-04-13 22:54:30 +0530

    Greedy algo added

--- src/Greedy_restart_tuf/Activity_Selection.java
+++ src/Greedy_restart_tuf/Activity_Selection.java
@@ -1,5 +1,41 @@
 package Greedy_restart_tuf;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+
+class Activity {
+    int start;
+    int end;
+    int pos;
+
+    Activity(int start, int end, int pos)
+    {
+        this.start = start;
+        this.end = end;
+        this.pos = pos;
+    }
+}
+
+class activityComparator implements Comparator<Activity>{
+
+    @Override
+    public int compare(Activity o1, Activity o2) {
+        if(o1.end<o2.end){
+            return -1;
+        } else if (o1.end>o2.end) {
+            return 1;
+        }
+        else if(o1.pos <)
+    }
+}
 public class Activity_Selection {
 
+    public int activitySelection(int start[], int end[], int n)
+    {
+        // add your code here
+        ArrayList<Activity> meet=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            meet.add(new Activity(start[i],end[i],i+1));
+        }
+    }
 }
