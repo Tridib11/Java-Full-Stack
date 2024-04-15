4a624ee31bbed3a78af65a51be9d47971ec4633d
Tridib11 <tridibghosh12345@gmail.com>
2024-04-16 00:30:42 +0530

    Greedy algo added

--- src/Greedy_Alpha/Activity_Selection_Unsorted.java
+++ src/Greedy_Alpha/Activity_Selection_Unsorted.java
@@ -6,8 +6,8 @@ import java.util.Comparator;
 
 public class Activity_Selection_Unsorted {
     public static void main(String[] args) {
-        int[] start={1,3,0,5,8,5};
-        int[] end={2,4,6,7,9,9};
+        int[] start={2,1};
+        int[] end={2,2};
         int[][] activities=new int[start.length][3];
         for(int i=0;i< start.length;i++){
             activities[i][0]=i;
@@ -20,6 +20,21 @@ public class Activity_Selection_Unsorted {
         ArrayList<Integer> ans=new ArrayList<>();
 
         ans.add(activities[0][0]);
+        int lastEnd=activities[0][2];
+
+        for(int i=1;i<end.length;i++){
+            if(activities[i][1]>=lastEnd){
+                maxAct++;
+                ans.add(activities[i][0]);
+                lastEnd=activities[i][2];
+            }
+        }
+
+        System.out.println("Max Activities "+maxAct);
+        for (int i = 0; i < ans.size(); i++) {
+            System.out.print("A"+ans.get(i)+" ");
+        }
+        System.out.println();
 
 
     }
