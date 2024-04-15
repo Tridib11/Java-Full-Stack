9b5c4022a353711dd38bf04a619c42e32828d9d8
Tridib11 <tridibghosh12345@gmail.com>
2024-04-15 23:57:05 +0530

    Greedy algo added

--- src/Greedy_Alpha/Activity_Selection_Unsorted.java
+++ src/Greedy_Alpha/Activity_Selection_Unsorted.java
@@ -2,6 +2,8 @@ package Greedy_Alpha;
 
 public class Activity_Selection_Unsorted {
     public static void main(String[] args) {
+        int[] start={1,3,0,5,8,5};
+        int[] end={2,4,6,7,9,9};
 
     }
 }
