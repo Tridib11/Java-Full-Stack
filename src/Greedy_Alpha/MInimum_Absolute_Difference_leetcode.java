1ec4e1b20873e4582beb4a0ba3dee7a225f27f54
Tridib11 <tridibghosh12345@gmail.com>
2024-04-17 02:47:18 +0530

    Greedy algo added

--- src/Greedy_Alpha/MInimum_Absolute_Difference_leetcode.java
+++ src/Greedy_Alpha/MInimum_Absolute_Difference_leetcode.java
@@ -1,9 +1,23 @@
 package Greedy_Alpha;
 import java.util.*;
+
+//https://leetcode.com/problems/minimum-absolute-difference/description/
 public class MInimum_Absolute_Difference_leetcode {
     class Solution {
         public List<List<Integer>> minimumAbsDifference(int[] arr) {
+                List<List<Integer>> newList=new ArrayList<>();
+                Arrays.sort(arr);
+                int min=Integer.MAX_VALUE;
+                for(int i=0;i<arr.length-1;i++){
+                    min=Math.min(min,Math.abs(arr[i]-arr[i+1]));
+                }
 
+                for(int i=0;i<arr.length-1;i++){
+                    if(Math.abs(arr[i]-arr[i+1])==min){
+                        newList.add(Arrays.asList(arr[i],arr[i+1]));
+                    }
+                }
+                return newList;
         }
     }
 }
