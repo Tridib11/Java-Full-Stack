e3d9d0c8e1888f3857cc7d156996d8004aed1bba
Tridib11 <tridibghosh12345@gmail.com>
2024-04-17 02:48:08 +0530

    Greedy algo added

--- src/Greedy_Alpha/Min_Absolute_Diff_GFG.java
+++ src/Greedy_Alpha/Min_Absolute_Diff_GFG.java
@@ -1,5 +1,6 @@
 package Greedy_Alpha;
 import java.util.*;
+//https://www.geeksforgeeks.org/problems/minimum-sum-of-absolute-differences-of-pairs/1?itm_source=geeksforgeeks&itm_medium=article&itm_campaign=bottom_sticky_on_article
 public class Min_Absolute_Diff_GFG {
 
     class Solution {
