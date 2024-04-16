7186020d33dbf7acf65323dac63c7d659e957bad
Tridib11 <tridibghosh12345@gmail.com>
2024-04-17 03:05:06 +0530

    Greedy algo added

--- src/Greedy_Alpha/Max_length_of_Pair_Chain.java
+++ src/Greedy_Alpha/Max_length_of_Pair_Chain.java
@@ -1,7 +1,20 @@
 package Greedy_Alpha;
 
-public class Max_length_of_Pair_Chain {
-    public int findLongestChain(int[][] pairs) {
+import java.util.Arrays;
+import java.util.Comparator;
 
+public class Max_length_of_Pair_Chain {
+    static int[][] demo={{5,24}, {39, 60}, {5, 28}, {27, 40}, {50, 90}};
+    public int findLongestChain(int[][] pairs) {
+        Arrays.sort(pairs, Comparator.comparingDouble(o->o[1]));
+        int chainLength=1;
+        int lastChainEnd=pairs[0][1];
+        for(int i=1;i<pairs.length;i++){
+            if(pairs[i][0]>lastChainEnd){
+                chainLength++;
+                lastChainEnd=pairs[i][1];
+            }
+        }
+        return chainLength;
     }
 }
