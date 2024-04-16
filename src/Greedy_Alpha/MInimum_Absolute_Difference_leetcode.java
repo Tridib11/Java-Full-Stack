a97304e8790470fc4d88fd1d6fec98c132214896
Tridib11 <tridibghosh12345@gmail.com>
2024-04-17 02:41:23 +0530

    Greedy algo added

---
+++ src/Greedy_Alpha/MInimum_Absolute_Difference_leetcode.java
@@ -0,0 +1,9 @@
+package Greedy_Alpha;
+import java.util.*;
+public class MInimum_Absolute_Difference_leetcode {
+    class Solution {
+        public List<List<Integer>> minimumAbsDifference(int[] arr) {
+
+        }
+    }
+}
