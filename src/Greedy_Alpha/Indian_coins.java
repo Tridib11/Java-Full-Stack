9de65127f38259c405e2b36045166c03e469f2ed
Tridib11 <tridibghosh12345@gmail.com>
2024-04-18 03:49:37 +0530

    Greedy algo added

--- src/Greedy_Alpha/Indian_coins.java
+++ src/Greedy_Alpha/Indian_coins.java
@@ -12,13 +12,13 @@ public class Indian_coins {
         int amount=590;
         ArrayList<Integer> ans=new ArrayList<>();
         for(int i =0;i<coins.length;i++){
-            if(coins[i]<=amount){
+
                 while(coins[i]<=amount){
                     countOfCoins++;
                     ans.add(coins[i]);
                     amount-=coins[i];
                 }
-            }
+
         }
         System.out.println(ans.size());
     }
