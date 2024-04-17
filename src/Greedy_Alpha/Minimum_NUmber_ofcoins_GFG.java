c305809169446ba70484e682a93255344c5fbf75
Tridib11 <tridibghosh12345@gmail.com>
2024-04-18 03:24:01 +0530

    Greedy algo added

--- src/Greedy_Alpha/Minimum_NUmber_ofcoins_GFG.java
+++ src/Greedy_Alpha/Minimum_NUmber_ofcoins_GFG.java
@@ -1,5 +1,25 @@
 package Greedy_Alpha;
 
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+class Solution{
+    static List<Integer> minPartition(int amount)
+    {
+         Integer[] coins={1,2,5,10,20,50,100,500,2000};
+         Arrays.sort(coins, Comparator.reverseOrder());
+         int countOfCoins=0;
+         List<Integer> ans=new ArrayList<>();
+         for(int i=0;i<coins.length;i++){
+             if()
+         }
+
+
+    }
+}
 public class Minimum_NUmber_ofcoins_GFG {
     public static void main(String[] args) {
 
