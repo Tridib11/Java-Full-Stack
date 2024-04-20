1d53310c9bee94fa76cf1db7419c06eb9a2eb155
Tridib11 <tridibghosh12345@gmail.com>
2024-04-20 14:21:44 +0530

    Binary trees added

--- src/Binary_Tree_Tuf/Inorder_Leetcode.java
+++ src/Binary_Tree_Tuf/Inorder_Leetcode.java
@@ -1,4 +1,37 @@
 package Binary_Tree_Tuf;
+import java.util.*;
+/*
+ * Definition for a binary tree node.
+ * https://leetcode.com/problems/binary-tree-inorder-traversal/
+ */
+  class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
 
+ class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res=new ArrayList<>();
+        inorder(root,res);
+        return res;
+    }
+    public void inorder(TreeNode node,List<Integer> res){
+        if(node==null){
+            return;
+        }
+        inorder(node.left,res);
+        res.add(node.val);
+        inorder(node.right,res);
+    }
+}
 public class Inorder_Leetcode {
+
 }
