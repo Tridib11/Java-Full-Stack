48719f3bce32ea9f3c51b6bb1ac4207dd411a15b
Tridib11 <tridibghosh12345@gmail.com>
2024-04-20 14:19:16 +0530

    Binary trees added

--- src/Binary_Tree_Tuf/Inorder_Traversal.java
+++ src/Binary_Tree_Tuf/Inorder_Traversal.java
@@ -1,7 +1,59 @@
 package Binary_Tree_Tuf;
 
-public class Inorder_Traversal {
-    public static void main(String[] args) {
+/*
+* (left ,root ,Right)
+*/
 
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Inorder_Traversal {
+
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;
+        }
+    }
+
+    public static void inorder(Node root,List<Integer> arr){
+        if(root==null){
+            return;
+        }
+        inorder(root.left,arr);
+        arr.add(root.data);
+        inorder(root.right,arr);
+    }
+
+    public static List<Integer> inOrder(Node root){
+        List<Integer> arr=new ArrayList<>();
+        inorder(root,arr);
+        return arr;
+
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+
+        // Getting inorder traversal
+        List<Integer> result = inOrder(root);
+
+        // Displaying the inorder traversal result
+        System.out.print("Inorder Traversal: ");
+        // Output each value in the
+        // inorder traversal result
+        for (int val : result) {
+            System.out.print(val + " ");
+        }
+        System.out.println();
     }
 }
