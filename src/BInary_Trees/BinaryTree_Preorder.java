10edff5e8ef4c9afa2ca7698c85e948745ed5d31
Tridib11 <tridibghosh12345@gmail.com>
2024-04-20 13:20:25 +0530

    Binary trees added

--- src/BInary_Trees/BinaryTree_Preorder.java
+++ src/BInary_Trees/BinaryTree_Preorder.java
@@ -1,6 +1,16 @@
 package BInary_Trees;
 
 public class BinaryTree_Preorder {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node (int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;
+        }
+    }
     public static void main(String[] args) {
         int[] nodes={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
     }
