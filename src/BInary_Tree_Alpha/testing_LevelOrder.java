a17185849bf4a60fa7dc099a74dfc4871bcf2bea
Tridib11 <tridibghosh12345@gmail.com>
2024-04-25 01:41:07 +0530

    Binary trees added

--- src/BInary_Tree_Alpha/testing_LevelOrder.java
+++ src/BInary_Tree_Alpha/testing_LevelOrder.java
@@ -26,8 +26,24 @@ public class testing_LevelOrder {
         Queue<Node> q=new LinkedList<>();
         q.add(root);
         q.add(null);
+        List<Integer> currentLevel = new ArrayList<>();
         while(!q.isEmpty()){
-
+            Node currNode=q.remove();
+            if(currNode==null){
+                result.add(currentLevel);
+                currentLevel=new ArrayList<>();
+                if(!q.isEmpty()){
+                    q.add(null);
+                }
+            }else{
+                currentLevel.add(currNode.data);
+                if(currNode.left!=null){
+                    q.add(currNode.left);
+                }
+                if(currNode.right!=null){
+                    q.add(currNode.right);
+                }
+            }
         }
 
     }
