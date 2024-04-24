eccac01c375f41a0070995bb74f12efbcc7f5f4d
Tridib11 <tridibghosh12345@gmail.com>
2024-04-24 10:28:47 +0530

    Binary trees added

---
+++ src/BInary_Tree_Alpha/Level_order_Traversal.java
@@ -0,0 +1,24 @@
+package BInary_Tree_Alpha;
+
+public class Level_order_Traversal {
+
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;
+        }
+    }
+
+    static void LevelOrder(Node root){
+        if(root==null){
+            return;
+        }
+    }
+
+
+
+}
