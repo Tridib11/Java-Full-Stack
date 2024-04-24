60e3023fc1f957d2a31b89c3ee71bf0e0941a893
Tridib11 <tridibghosh12345@gmail.com>
2024-04-25 01:14:21 +0530

    Binary trees added

---
+++ src/BInary_Tree_Alpha/testing_LevelOrder.java
@@ -0,0 +1,15 @@
+package BInary_Tree_Alpha;
+
+
+public class testing_LevelOrder {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+        Node (int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;
+        }
+    }
+}
