b6af9ba4110ce216e0c1056630b3c81ca4b9e6e1
Tridib11 <tridibghosh12345@gmail.com>
2024-04-24 09:40:50 +0530

    Binary trees added

---
+++ src/Binary_Tree_Tuf/LevelOrderTraversal_Leetcode.java
@@ -0,0 +1,21 @@
+package Binary_Tree_Tuf;
+import java.util.*;
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+public class LevelOrderTraversal_Leetcode {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+
+    }
+
+
+}
