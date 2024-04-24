e46960df526a1d041a3b43478d262add0a257ca2
Tridib11 <tridibghosh12345@gmail.com>
2024-04-24 10:08:34 +0530

    Binary trees added

--- src/Binary_Tree_Tuf/LevelOrderTraversal_iterative_Leetcode.java
+++ src/Binary_Tree_Tuf/LevelOrderTraversal_iterative_Leetcode.java
@@ -1,6 +1,6 @@
 package Binary_Tree_Tuf;
 import com.sun.source.tree.Tree;
-
+//https://leetcode.com/problems/binary-tree-level-order-traversal/
 import java.util.*;
 class TreeNode {
     int val;
@@ -19,15 +19,23 @@ public class LevelOrderTraversal_iterative_Leetcode {
         List<List<Integer>> result = new ArrayList<>();
         if(root == null) return result;
         Queue<TreeNode> q=new LinkedList<>();
-        q.add(root);
+        q.offer(root);
         while(!q.isEmpty()){
             int size=q.size();
             List<Integer> currLevel=new ArrayList<>();
-            while(size-- > 0){
+            for(int i=0;i<size;i++){
                 TreeNode curr=q.poll();
-
+                currLevel.add(curr.val);
+                if(curr.left!=null){
+                    q.offer(curr.left);
+                }
+                if(curr.right!=null){
+                    q.offer(curr.right);
+                }
             }
+            result.add(currLevel);
         }
+        return result;
     }
 
 
