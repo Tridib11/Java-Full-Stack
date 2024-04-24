7883510713ff40d4e9342f287a933cb7ad020a13
Tridib11 <tridibghosh12345@gmail.com>
2024-04-24 11:51:45 +0530

    Binary trees added

--- src/BInary_Tree_Alpha/Level_order_Traversal.java
+++ src/BInary_Tree_Alpha/Level_order_Traversal.java
@@ -1,5 +1,8 @@
 package BInary_Tree_Alpha;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Level_order_Traversal {
 
     static class Node{
@@ -17,6 +20,35 @@ public class Level_order_Traversal {
         if(root==null){
             return;
         }
+        Queue<Node> q=new LinkedList<>();
+        q.add(root);
+        q.add(null);
+        while(!q.isEmpty()){
+            Node currNode=q.remove();
+            if(currNode==null){
+                System.out.println();
+                if(q.isEmpty()){
+                    break;
+                }
+                else{
+                   q.add(null);
+                }
+            }else{
+                System.out.println(currNode.data+" ");
+                if(currNode.left!=null){
+                    q.add(currNode.left);
+                }
+                if(currNode.right!=null){
+                    q.add(currNode.right);
+                }
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int[] nodes={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+        
     }
 
 
