aed070effaf5c116cd778eeaa71bdfe930e0b71a
Tridib11 <tridibghosh12345@gmail.com>
2024-04-25 10:46:16 +0530

    Binary trees added

---
+++ src/BInary_Tree_Alpha/Count_Number_Of_Nodes.java
@@ -0,0 +1,26 @@
+package BInary_Tree_Alpha;
+
+import Binary_Tree_Tuf.Height_Of_The_BinaryTree;
+
+public class Count_Number_Of_Nodes {
+    class Node {
+        int data;
+        Node left;
+        Node right;
+        Node(int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;
+        }
+    }
+    public int countNodes(Node root) {
+        if(root==null){
+            return 0;
+        }
+        int lh=countNodes(root.left);
+        int rh=countNodes(root.right);
+        return lh+rh+1;
+
+    }
+
+}
