556323abeafade0e87eb8a9a2d3eeb8eb1b09091
Tridib11 <tridibghosh12345@gmail.com>
2024-04-25 10:36:25 +0530

    Binary trees added

---
+++ src/Binary_Tree_Tuf/Height_Of_The_BinaryTree.java
@@ -0,0 +1,24 @@
+package Binary_Tree_Tuf;
+
+public class Height_Of_The_BinaryTree {
+    class Node {
+        int data;
+        Node left;
+        Node right;
+        Node(int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;
+        }
+    }
+
+
+    public int maxDepth(Node root){
+        if(root==null){
+            return 0;
+        }
+        int lh= maxDepth(root.left);
+        int rh= maxDepth(root.right);
+        return 1+ Math.max(lh,rh);
+    }
+}
