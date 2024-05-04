fc38225c6fee2650992e1b664c18d2d4197e2122
Tridib11 <tridibghosh12345@gmail.com>
2024-05-05 03:45:46 +0530

    Binary trees added

--- src/Binary_Tree_AnujBHaiya/Bottom_view_of_a_Binary_Tree.java
+++ src/Binary_Tree_AnujBHaiya/Bottom_view_of_a_Binary_Tree.java
@@ -1,9 +1,34 @@
 package Binary_Tree_AnujBHaiya;
-
+import java.util.*;
 public class Bottom_view_of_a_Binary_Tree {
     static class Node{
         int data;
         Node left;
-
+        Node right;
+        Node(int data,Node left,Node right){
+            this.data=data;
+            this.left=left;
+            this.right=right;
+        }
     }
+
+
+    class Pair{
+        int horizontal_distance;
+        Node node;
+        Pair(int horizontal_distance,Node node){
+            this.horizontal_distance=horizontal_distance;
+            this.node=node;
+        }
+    }
+
+    class Solution{
+        public ArrayList <Integer> bottomView(Node root)
+        {
+            Queue<Pair> queue=new ArrayDeque<>();
+
+        }
+    }
+
+
 }
