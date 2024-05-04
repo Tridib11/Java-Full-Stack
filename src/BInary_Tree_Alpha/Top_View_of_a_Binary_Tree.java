74cea4f7474fb53be33bcfc9f0893617da734ef0
Tridib11 <tridibghosh12345@gmail.com>
2024-05-05 03:10:11 +0530

    Binary trees added

--- src/BInary_Tree_Alpha/Top_View_of_a_Binary_Tree.java
+++ src/BInary_Tree_Alpha/Top_View_of_a_Binary_Tree.java
@@ -1,30 +1,35 @@
 package BInary_Tree_Alpha;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class Top_View_of_a_Binary_Tree {
-    static class Node{
+    static class Node {
         int data;
         Node left;
         Node right;
-        Node(int data){
-            this.data=data;
-            this.left=null;
-            this.right=null;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
         }
     }
-    static class Pair{
+
+    static class Pair {
         int horizontal_Distance;
         Node node;
-        Pair(int horizontal_Distance,Node node){
-            this.node=node;
-            this.horizontal_Distance=horizontal_Distance;
+
+        Pair(int horizontal_Distance, Node node) {
+            this.node = node;
+            this.horizontal_Distance = horizontal_Distance;
         }
     }
 
 
-    class Solution{
-        static ArrayList<Integer> topView(Node root){
+    class Solution {
+        static ArrayList<Integer> topView(Node root) {
+            Queue<Integer> queue = new ArrayDeque<>();
+            Map<Integer, Integer> map = new TreeMap<>(); //used to store data in sorted order of keys
 
         }
     }
