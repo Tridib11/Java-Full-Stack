4968579a791a358cd90a24d074de7fd402b27d9b
Tridib11 <tridibghosh12345@gmail.com>
2024-05-05 04:03:54 +0530

    Binary trees added

--- src/Binary_Tree_AnujBHaiya/Bottom_view_of_a_Binary_Tree.java
+++ src/Binary_Tree_AnujBHaiya/Bottom_view_of_a_Binary_Tree.java
@@ -7,28 +7,43 @@ public class Bottom_view_of_a_Binary_Tree {
         Node right;
         Node(int data,Node left,Node right){
             this.data=data;
-            this.left=left;
-            this.right=right;
+            this.left=null;
+            this.right=null;
         }
     }
 
 
-    class Pair{
-        int horizontal_distance;
+    static class Pair {
+        int horizontal_Distance;
         Node node;
-        Pair(int horizontal_distance,Node node){
-            this.horizontal_distance=horizontal_distance;
-            this.node=node;
+
+        Pair(int horizontal_Distance, Node node) {
+            this.node = node;
+            this.horizontal_Distance = horizontal_Distance;
         }
     }
 
     class Solution{
-        public ArrayList <Integer> bottomView(Node root)
+        public static ArrayList <Integer> bottomView(Node root)
         {
             Queue<Pair> queue=new ArrayDeque<>();
             Map<Integer,Integer> map=new TreeMap<>();
             queue.add(new Pair(0,root));
-
+            while(!queue.isEmpty()){
+                Pair curr=queue.poll();
+                map.put(curr.horizontal_Distance,curr.node.data);
+                if(curr.node.left!=null){
+                    queue.add(new Pair(curr.horizontal_Distance-1,curr.node.left));
+                }
+                if(curr.node.right!=null){
+                    queue.add(new Pair(curr.horizontal_Distance+1,curr.node.right));
+                }
+            }
+            ArrayList<Integer> ans=new ArrayList<>();
+            for(Map.Entry<Integer,Integer> entry:map.entrySet()){
+                ans.add(entry.getValue());
+            }
+            return ans;
         }
     }
 
