73310a3a0c2dee6177bf6afdbbba4e0eaccd170b
Tridib11 <tridibghosh12345@gmail.com>
2024-05-05 03:07:04 +0530

    Binary trees added

--- src/BInary_Tree_Alpha/Top_View_of_a_Binary_Tree.java
+++ src/BInary_Tree_Alpha/Top_View_of_a_Binary_Tree.java
@@ -1,5 +1,31 @@
 package BInary_Tree_Alpha;
 
-public class Top_View_of_a_Binary_Tree {
+import java.util.ArrayList;
 
+public class Top_View_of_a_Binary_Tree {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;
+        }
+    }
+    static class Pair{
+        int horizontal_Distance;
+        Node node;
+        Pair(int horizontal_Distance,Node node){
+            this.node=node;
+            this.horizontal_Distance=horizontal_Distance;
+        }
+    }
+
+
+    class Solution{
+        static ArrayList<Integer> topView(Node root){
+
+        }
+    }
 }
