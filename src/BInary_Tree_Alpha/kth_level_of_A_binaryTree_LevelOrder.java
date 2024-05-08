a8742114785c2365ebdb2e9340db02bc27a7c63c
Tridib11 <tridibghosh12345@gmail.com>
2024-05-08 22:44:59 +0530

    Binary trees added

--- src/BInary_Tree_Alpha/kth_level_of_A_binaryTree_LevelOrder.java
+++ src/BInary_Tree_Alpha/kth_level_of_A_binaryTree_LevelOrder.java
@@ -1,5 +1,9 @@
 package BInary_Tree_Alpha;
 
+import java.util.List;
+
+import static BInary_Tree_Alpha.kth_level_of_A_binaryTree_Preorder.kthLevelNodes;
+
 public class kth_level_of_A_binaryTree_LevelOrder {
     static class Node{
         int data;
@@ -10,6 +14,18 @@ public class kth_level_of_A_binaryTree_LevelOrder {
             this.left=null;
             this.right=null;
         }
+    }
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.right = new Node(6);
+
+        int k = 3; // Specify the level you want to find
+        List<Integer> result = kthLevelNodes(root, k);
+        System.out.println("Nodes at level " + k + ": " + result);
     }
 
 
