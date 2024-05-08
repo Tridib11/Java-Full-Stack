46f38c292bb0e9ffc0f36cea21318f32b2f45dad
Tridib11 <tridibghosh12345@gmail.com>
2024-05-08 22:43:46 +0530

    Binary trees added

---
+++ src/BInary_Tree_Alpha/kth_level_of_A_binaryTree_LevelOrder.java
@@ -0,0 +1,16 @@
+package BInary_Tree_Alpha;
+
+public class kth_level_of_A_binaryTree_LevelOrder {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;
+        }
+    }
+
+
+}
