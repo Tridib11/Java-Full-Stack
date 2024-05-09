b9858a9c52ffa2eb1aeb471ec660dfc3434b8aa8
Tridib11 <tridibghosh12345@gmail.com>
2024-05-09 09:49:15 +0530

    Binary trees added

--- src/Binary_Tree_AnujBHaiya/Lowest_Common_Ancestor.java
+++ src/Binary_Tree_AnujBHaiya/Lowest_Common_Ancestor.java
@@ -1,5 +1,8 @@
 package Binary_Tree_AnujBHaiya;
 
+
+import java.util.*;
+
 public class Lowest_Common_Ancestor {
     static class Node{
         int data;
@@ -10,6 +13,16 @@ public class Lowest_Common_Ancestor {
             this.right=null;
             this.left=null;
         }
+    }
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.right = new Node(6);
+
+
     }
 
 
