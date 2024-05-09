a9cb4d5f369361ad6a3ff6230ea42fb292f1cfb6
Tridib11 <tridibghosh12345@gmail.com>
2024-05-09 10:33:23 +0530

    Binary trees added

--- src/Binary_Tree_AnujBHaiya/Lowest_Common_Ancestor_Leetcode.java
+++ src/Binary_Tree_AnujBHaiya/Lowest_Common_Ancestor_Leetcode.java
@@ -1,4 +1,33 @@
 package Binary_Tree_AnujBHaiya;
 
+//https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
+import java.util.*;
 public class Lowest_Common_Ancestor_Leetcode {
+    static class TreeNode{
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int val){
+            this.val=val;
+            this.left=null;
+            this.right=null;
+        }
+    }
+    public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root==null) return null;
+        if(root.val==p.val || root.val==q.val) return root;
+
+        TreeNode left=lowestCommonAncestor(root.left,p,q);
+        TreeNode right=lowestCommonAncestor(root.right,p,q);
+
+        if(left==null) return right;
+        if(right==null) return left;
+
+        return root;
+    }
+
+
+
+
+
 }
