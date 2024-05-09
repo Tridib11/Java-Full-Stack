e1944cbec017e4c554c48a7f7e5513d2962b4fe9
Tridib11 <tridibghosh12345@gmail.com>
2024-05-09 10:54:10 +0530

    Binary trees added

---
+++ src/BInary_Tree_Alpha/Min_DIstance_Between_Two_Nodes.java
@@ -0,0 +1,17 @@
+package BInary_Tree_Alpha;
+
+public class Min_DIstance_Between_Two_Nodes {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;
+        }
+    }
+
+
+
+}
